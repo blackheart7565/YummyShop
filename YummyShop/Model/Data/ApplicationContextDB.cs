874a0aa4d2bbb50e91014b5eaed0737f2cc2befd
blackheart7565 <give.me.heart8844@gmail.com>
2023-03-02 22:39:35 +0200

    Add project files.

---
+++ YummyShop/Model/Data/ApplicationContextDB.cs
@@ -0,0 +1,22 @@
+﻿using Accessibility;
+using Microsoft.EntityFrameworkCore;
+using YummyShop.Model.DataBaseTableModel;
+
+namespace YummyShop.Model.Data {
+    /// <summary>
+    /// Класс подключения к БД || создание БД
+    /// </summary>
+
+    public class ApplicationContextDB : DbContext {
+
+        DbSet<Users> Users { get; set; }
+
+        public ApplicationContextDB() {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-CI6CQHH\\SQL_BLACK_HEART;Initial Catalog=YummyShopDataBase;User ID=sa;Password=55986;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
